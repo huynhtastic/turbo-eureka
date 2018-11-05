8e8f55dbe66e1dce4805d42627bd794721d94781
Richard Huynh <huynhtastic@gmail.com>
2018-11-04 18:14:58 -0600

    employees page now reads selections given from database

--- src/containers/Employee.js
+++ src/containers/Employee.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Cookies from 'universal-cookie';
-import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import fetch from 'node-fetch';
 import "./Employee.css";
 
 const cookies = new Cookies();
@@ -12,11 +13,37 @@ export default class Employee extends Component {
     this.state = {
       receivedBalance: 0,
       giveableBalance: 0,
+      recipient: "",
+      amount: 1,
+      giveableEmployees: undefined,
+    }
+  }
+
+  validateForm() {
+    return this.state.recipient === "";
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  populateEmployees() {
+    if (this.state.giveableEmployees) {
+      return this.state.giveableEmployees.map((emp) =>
+        <option key={emp.USERNAME} readOnly="readonly" value={emp.USERNAME}>
+          {emp.FIRST_NAME} {emp.LAST_NAME}
+        </option>
+      );
     }
   }
 
   componentDidMount() {
-    console.log('asdf');
     fetch(`http://localhost:3001/api/balances/${cookies.get('emp_id')}`)
       .then((res) => {
         if (res.status === 404) {
@@ -27,12 +54,22 @@ export default class Employee extends Component {
         }
       })
       .then((json) => {
-        console.log(json);
         this.setState({
           receivedBalance: json.POINTS_RECEIVED,
           giveableBalance: json.POINTS_GIVEABLE,
         });
-        console.log(this.state);
+      });
+    fetch(`http://localhost:3001/api/employees/${cookies.get('emp_id')}`)
+      .then((res) => {
+        if (res.status === 404) {
+          return {};
+        } else {
+          return res.json();
+        }
+      })
+      .then((json) => {
+        console.log(json);
+        this.setState({giveableEmployees: json});
       });
   }
 
@@ -41,21 +78,48 @@ export default class Employee extends Component {
       <div className="Employee">
         <form onSubmit={this.handleSubmit}>
           <FormGroup controlId="current" bsSize="large">
-            <ControlLabel>Current Balance</ControlLabel>
+            <ControlLabel>Received Balance</ControlLabel>
             <FormControl
               type="number"
               value={this.state.receivedBalance}
               readOnly="readonly"
             />
           </FormGroup>
-          <FormGroup controlId="kudos" bsSize="large">
-            <ControlLabel>Kudos Balance</ControlLabel>
+          <FormGroup controlId="giveable" bsSize="large">
+            <ControlLabel>Giveable Balance</ControlLabel>
             <FormControl
               type="number"
               value={this.state.giveableBalance}
               readOnly="readonly"
             />
           </FormGroup>
+          <FormGroup controlId="recipient" bsSize="large">
+            <ControlLabel>Give points to:</ControlLabel>
+            <FormControl
+              componentClass="select"
+              placeholder="Employee"
+              onChange={this.handleChange}
+            >
+              {this.populateEmployees()}
+            </FormControl>
+          </FormGroup>
+          <FormGroup controlId="amount" bsSize="large">
+            <ControlLabel>Give amount:</ControlLabel>
+            <FormControl
+              min="1"
+              type="number"
+              onChange={this.handleChange}
+              value={this.state.amount}
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Give Points
+          </Button>
         </form>
       </div>
     )
